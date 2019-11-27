283f28617d10285eff37acb67820b9ad523b6994
Maria Metrina <maria.metrina@gmail.com>
2019-11-26 20:46:02 -0500

    Initial Commit

---
+++ Simple_ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple_ToDoApp
+//
+//  Created by Vishal Patel on 2019-11-26.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
